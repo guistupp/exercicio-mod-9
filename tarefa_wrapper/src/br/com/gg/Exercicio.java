834540f399e4f7ccd1ef135dfef9a6d2bbd0e34e
guistupp <guistiipp@gmail.com>
2024-04-22 20:54:42 -0300

    first commit

---
+++ tarefa_wrapper/src/br/com/gg/Exercicio.java
@@ -0,0 +1,14 @@
+package br.com.gg;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+
+		int num = 42;
+		System.out.println(num);
+
+		Integer num2 = Integer.valueOf(num);
+		System.out.println(num2);
+	}
+
+}
